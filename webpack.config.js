8500168bc17648e2885a5a6f95744d7254e5014c
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-12-18 13:42:51 -0600

    add offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const path = require("path");
 
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './'),
+    },
+    compress: true,
+    port: 8080,
+  },
   entry: {
     app: './assets/js/script.js',
     events: './assets/js/events.js',
